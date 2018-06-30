a4f3037a916b7e618e2ca1c333a5e12a81c5868c
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-30 23:47:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
-    get '/' do 
+    get '/' do
       erb :super_hero
     end
 
-    post '/teams' do 
+    post '/teams' do
 
       erb :team
     end
